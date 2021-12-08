98289270924082dd05b44ac064305e116fd96d30
woodonggyu <woodonggyu92@gmail.com>
2021-12-09 03:19:31 +0900

    docs: Add example codes    + ManagedRuleGroupStatement    + Logical Statement (And, Or, Not)    + GeoMatchStatement    + IPSetReferenceStatement    + RateBasedStatement    + SizeConstraintStatement    + SqliMatchStatement    + XssMatchStatement

---
+++ examples/SizeConstraintStatement/main.tf
@@ -0,0 +1,45 @@
+provider "aws" { region = "ap-northeast-2" }
+
+module "wafv2" {
+  source  = "../.."
+  name    = "WebACL01"
+  scope   = "REGIONAL"
+
+  rules = [
+    {
+      name     = "SizeConstraintRule01"
+      priority = 10
+      action   = "count"
+      size_constraint_statement = {
+        field_to_match = {
+          single_header = {
+            name = "host"
+          }
+        }
+        comparison_operator = "EQ"
+        size                = 10
+        text_transformation = {
+          priority  = 20
+          type      = "NONE"
+        }
+      }
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        metric_name                = "cloudwatch_wafv2_metrics"
+        sampled_requests_enabled   = false
+      }
+    }
+  ]
+
+  visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "cloudwatch_wafv2_metrics"
+    sampled_requests_enabled   = false
+  }
+
+  tags = {
+    "Name": "PROD.WAFv2"
+    "Team": "Security Engineering"
+    "Owner": "Donggyu Woo"
+  }
+}

0ff67fb840cba9ea99ed4796b5ced24ffc19c0e7
woodonggyu <woodonggyu92@gmail.com>
2021-12-10 02:49:40 +0900

    Update Provision Instructions from local to remote

--- examples/XssMatchStatement/main.tf
+++ examples/XssMatchStatement/main.tf
@@ -1,7 +1,9 @@
 provider "aws" { region = "ap-northeast-2" }
 
 module "wafv2" {
-  source  = "../.."
+  source  = "woodonggyu/wafv2/aws"
+  version = "2.0.0"
+
   name    = "WebACL01"
   scope   = "REGIONAL"
 

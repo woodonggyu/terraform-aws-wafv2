876c48607d6d3d4eda6573dc966d127d7d82a13c
woodonggyu <woodonggyu92@gmail.com>
2021-12-10 15:51:02 +0900

    Update example codes    + ByteMatchStatement    + LabelMatchStatement

---
+++ examples/ByteMatchStatement/main.tf
@@ -0,0 +1,52 @@
+provider "aws" { region = "ap-northeast-2" }
+
+module "wafv2" {
+  source  = "../.."
+
+  name    = "WebACL01"
+  scope   = "REGIONAL"
+
+  rules = [
+    {
+      name     = "ByteMatchRule01"
+      priority = 10
+      action   = "count"
+      byte_match_statement = {
+        field_to_match = {
+          single_header = {
+            name = "host"
+          }
+        }
+        positional_constraint = "EXACTLY"
+        search_string         = "test"
+        text_transformation = {
+          priority  = 20
+          type      = "NONE"
+        }
+      }
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        metric_name                = "cloudwatch_wafv2_metrics"
+        sampled_requests_enabled   = false
+      }
+    }
+  ]
+
+  visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "cloudwatch_wafv2_metrics"
+    sampled_requests_enabled   = false
+  }
+
+  tags = {
+    "Name": "PROD.WAFv2"
+    "Team": "Security Engineering"
+    "Owner": "Donggyu Woo"
+  }
+
+  enable_logging_configuration  = false
+  log_destination_configs       = []
+
+  enable_webacl_association = false
+  alb_resource_arn          = []
+}
---
+++ examples/LabelMatchStatement/main.tf
@@ -0,0 +1,43 @@
+provider "aws" { region = "ap-northeast-2" }
+
+module "wafv2" {
+  source  = "../.."
+
+  name    = "WebACL01"
+  scope   = "REGIONAL"
+
+  rules = [
+    {
+      name     = "ByteMatchRule01"
+      priority = 10
+      action   = "count"
+      label_match_statement = {
+        key   = ""
+        scope = "LABEL"
+      }
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        metric_name                = "cloudwatch_wafv2_metrics"
+        sampled_requests_enabled   = false
+      }
+    }
+  ]
+
+  visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "cloudwatch_wafv2_metrics"
+    sampled_requests_enabled   = false
+  }
+
+  tags = {
+    "Name": "PROD.WAFv2"
+    "Team": "Security Engineering"
+    "Owner": "Donggyu Woo"
+  }
+
+  enable_logging_configuration  = false
+  log_destination_configs       = []
+
+  enable_webacl_association = false
+  alb_resource_arn          = []
+}
